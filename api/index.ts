a4f2c5ac921b802c80c9c63f1a69d8de2b542be6
Juan Ignacio Bayúgar <juanibayugar@gmail.com>
2026-08-10 21:31:01 -0300

    otra vez

--- api/index.ts
+++ api/index.ts
@@ -1,32 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common'; // <-- Agregado
 import { AppModule } from '../src/app.module';
-import express from 'express';
 
-const server = express();
+export default async function handler(req: any, res: any) {
+  const app = await NestFactory.create(AppModule);
 
-export const createNestServer = async (expressInstance: express.Express) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
-  
-  // Habilitar CORS
   app.enableCors({
-    origin: '*', // O la URL de tu React
-    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
   });
 
-  // Habilitar las validaciones globales (como en tu main.ts)
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
-
   await app.init();
-};
 
-createNestServer(server);
-
-export default server;
+  const expressApp = app.getHttpAdapter().getInstance();
+  return expressApp(req, res);
+}
